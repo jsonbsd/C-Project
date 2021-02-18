f30ccd2b736248e6f85e99c8d7f7b082edefe1b6
jsonbsd <jsonbsd@gmail.com>
2021-02-18 21:57:53 +0900

    Initial commit

---
+++ C Project/main.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ * C Project
+ *
+ * Created by ljy on 2021/02/08.
+ * Copyright © 2021 ljy. All rights reserved.
+ *
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+#include <float.h>
+#include <errno.h>
+#include <math.h>
+
+int main(int argc, const char *argv[]) {
+    
+    
+    return 0;
+}
